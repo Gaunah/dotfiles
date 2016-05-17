e094f72cce159ab23ba23a51a87a57fbc8d5c534
gaunah <christoph@zelinski.de>
2016-05-17 08:35:29 +0200

    added echo to fetch_mirrorlist

--- scripts/fetch_mirrorlist.sh
+++ scripts/fetch_mirrorlist.sh
@@ -1,1 +1,2 @@
 wget "https://www.archlinux.org/mirrorlist/?country=DE&protocol=https&ip_version=4&ip_version=6&use_mirror_status=on" -O - | sed 's/#//' > mirrorlist && sudo mv mirrorlist /etc/pacman.d/mirrorlist 
+echo mirrorlist written to /etc/pacman.d/mirrorlist
