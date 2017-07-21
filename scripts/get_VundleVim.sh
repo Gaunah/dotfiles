9bbdf39c9218d6d6569b810c40940c33702ef3b1
gaunah <christoph@zelinski.de>
2017-07-21 16:14:55 +0200

    improve get vundle script

--- scripts/get_VundleVim.sh
+++ scripts/get_VundleVim.sh
@@ -1,10 +1,13 @@
-#!/bin/bash
 
-if ! [ -d ~/.vim/bundle/Vundle.vim ] ; then
-	git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-else
-	echo "~/.vim/bundle/Vundle.vim already exists!"
-	exit 1
+#check for git
+if ! command -v git >/dev/null; then
+    echo "git needs to be installed!"; exit 1
 fi
 
-exit 0
+if ! [ -d ~/.vim/bundle/Vundle.vim ] ; then
+    git clone --depth 1 https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+else
+    echo "~/.vim/bundle/Vundle.vim already exists!"; exit 1
+#!/bin/bash
+fi
+
