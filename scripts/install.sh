49b4b2ed7b9a9e46356fde4fd5769510509677b6
gaunah <christoph@zelinski.de>
2019-04-03 09:19:27 +0200

    make install script more verbose

--- scripts/install.sh
+++ scripts/install.sh
@@ -4,8 +4,8 @@ pushd $(dirname $0) > /dev/null # move to script folder
 cd ..
 DOTFILE_DIR=$(pwd)
 
-find -name ".*" -type f | xargs -I{} cp -fi {} ~
-cp -ri .vim ~
+find -name ".*" -type f | xargs -I{} cp -vfi {} ~
+cp -vri .vim ~
 scripts/get_VundleVim.sh
 
 popd > /dev/null
